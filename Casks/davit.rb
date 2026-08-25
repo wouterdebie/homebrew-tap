98d2f5c217c48c18bba06e2f3d18d811048d50fe
davit-release <noreply@davit.app>
2026-08-25 21:21:38 +0000

    davit 0.1.22

--- Casks/davit.rb
+++ Casks/davit.rb
@@ -1,6 +1,6 @@
 cask "davit" do
-  version "0.1.21"
-  sha256 "a04af45a4be03cb0d3abf7d7f66b6f87998aee3deb984e7d563a933c932ff11a"
+  version "0.1.22"
+  sha256 "6b53175cbda3572313c392d86c47f3864523bb622f7b5dfcb4f9ccfea7d3b446"
 
   url "https://github.com/wouterdebie/davit/releases/download/v#{version}/Davit-#{version}.zip",
       verified: "github.com/wouterdebie/davit/"
